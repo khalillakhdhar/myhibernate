7590cd3f238fa27cf5c58b5552986e3415c9fdf6
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-10-30 15:27:37 +0100

    commit

--- src/main/java/com/projet/model/entities/User.java
+++ src/main/java/com/projet/model/entities/User.java
@@ -1,5 +1,85 @@
 package com.projet.model.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Entity(name = "utilisateur")
 public class User {
 
+	@Id
+	private long id;
+	@Column(nullable = false)
+	private String nom;
+	@Column(nullable = false)
+	private String prenom;
+	@NotBlank
+	@Column(length = 20)
+	String tel;
+	@Min(value = 18)
+	@Max(value = 34)
+	int age;
+	@Column(unique = true)
+	private String login;
+	@Column(nullable = true)
+	private String adresse;
+	public User(long id, String nom, String prenom, String login, String adresse) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.login = login;
+		this.adresse = adresse;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 }
